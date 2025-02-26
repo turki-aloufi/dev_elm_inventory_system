07a734b8a18b96b2fe56365dcf383d73063e2f9e
turki-aloufi <turkialoufi44@gmail.com>
2025-02-26 14:15:22 +0300

    OrderManager.java

--- src/OrderManager.java
+++ src/OrderManager.java
@@ -8,10 +8,10 @@ class OrderManager {
     private List<Order> orders = new ArrayList<>();
 
     public void processOrder(Order order, Product product) {
-        product.reduceStock(order.getQuantity());
-        orders.add(order);
-        System.out.println("Processing order: " + order.getOrderId());
-        executorService.submit(() -> updateOrderStatus(order));
+        if (product.getStockLevel() < order.getQuantity()) {
+            System.out.println("Order " + order.getOrderId() + " cannot be processed due to insufficient stock.");
+            return;
+        }
 
         product.reduceStock(order.getQuantity());
         orders.add(order);
